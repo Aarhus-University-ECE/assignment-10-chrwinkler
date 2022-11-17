c46b5adeef8fc75e216b9187e9b87f45dd8ee26e
chrwinkler <101516854+chrwinkler@users.noreply.github.com>
2022-11-17 14:59:03 +0100

    finnish assignment

--- src/linked_list.c
+++ src/linked_list.c
@@ -26,12 +26,28 @@ void free_list(node *p) {
 void print_list(node *p) {
   // Add your code for exercise 1
   // There is NO testcode for this
+  // It wil continue to print until = NULL
+  while(p != NULL)
+  {
+    printf("%d", p->value);
+    p = p->next;
+  }
+
 }
 
 int sum_squares(node *p) {
   // Add your code for excercise 2
   // You can find the tests in tests.cpp
-  return -1;
+  int sum = 0;
+
+// we time the number togehter two time, and then put it together to have number squares.
+// this number is added to the sum.
+  while(p != NULL)
+  {
+    sum += square(p->value);
+    p=p->next;
+  }
+  return sum;
 }
 
 typedef int (*fn_int_to_int)(int);
@@ -39,8 +55,20 @@ typedef int (*fn_int_to_int)(int);
 node *map(node *p, fn_int_to_int f) { 
   // Add your code for excercise 3
   
-  return NULL; 
+  int r;
+
+  // pre
+  assert(p!= NULL);
+
+  f = square
+
+  for(; p!=NULL; p = p->next)
+    r = f(p->value);
+  
+return r;
 }
+  
+
 
 int square(int x) { return x * x; }
 
